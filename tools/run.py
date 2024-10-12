9f8541d3a9633565a56440add5018e17ed6ce5a7
Stephen Moore <stephen@delfick.com>
2024-10-12 20:58:00 +1100

    Adding mypy to the project

--- tools/run.py
+++ tools/run.py
@@ -51,6 +51,36 @@ def lint(args: list[str]) -> None:
 
 @cli.command(context_settings=dict(ignore_unknown_options=True))
 @click.argument("args", nargs=-1, type=click.UNPROCESSED)
+def types(args: list[str]) -> None:
+    """
+    Run mypy
+    """
+    locations: list[str] = [a for a in args if not a.startswith("-")]
+    args = [a for a in args if a.startswith("-")]
+
+    if not locations:
+        locations.append(str((here / "..").resolve()))
+    else:
+        cwd = pathlib.Path.cwd()
+        paths: list[pathlib.Path] = []
+        for location in locations:
+            from_current = cwd / location
+            from_root = here.parent / location
+
+            if from_current.exists():
+                paths.append(from_current)
+            elif from_root.exists():
+                paths.append(from_root)
+            else:
+                raise ValueError(f"Couldn't find path for {location}")
+
+        locations = [str(path) for path in paths]
+
+    run("python", "-m", "mypy", *locations, *args)
+
+
+@cli.command(context_settings=dict(ignore_unknown_options=True))
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
 def tests(args: list[str]) -> None:
     """
     Run pytest
