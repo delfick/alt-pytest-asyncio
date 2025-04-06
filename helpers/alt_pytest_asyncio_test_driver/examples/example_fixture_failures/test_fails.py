ab5c953b740796c5aaa82509267c99458a60b5d7
Stephen Moore <stephen@delfick.com>
2025-04-06 15:12:10 +1000

    Add a test showing fixture errors do not persist

--- helpers/alt_pytest_asyncio_test_driver/examples/example_fixture_failures/test_fails.py
+++ helpers/alt_pytest_asyncio_test_driver/examples/example_fixture_failures/test_fails.py
@@ -30,6 +30,44 @@ async def fixture_fails_in_finally() -> AsyncGenerator[int]:
         await one()
 
 
+_done_iterator: bool = False
+_done_result: bool = False
+_done_finally: bool = False
+
+
+@pytest.fixture()
+async def fixture_fails_once_iterator() -> AsyncGenerator[None]:
+    global _done_iterator
+    if _done_iterator:
+        return
+
+    _done_iterator = True
+    yield await one()
+
+
+@pytest.fixture()
+async def fixture_fails_once_result() -> None:
+    global _done_result
+    if _done_result:
+        return
+
+    _done_result = True
+    await one()
+
+
+@pytest.fixture()
+async def fixture_fails_once_finally() -> AsyncGenerator[None]:
+    try:
+        yield
+    finally:
+        global _done_finally
+        if _done_finally:
+            return
+
+        _done_finally = True
+        await one()
+
+
 def test_fails_on_fixture_returns(fixture_returns: int) -> None:
     pass
 
@@ -39,4 +77,40 @@ def test_fails_on_fixture_yields(fixture_yields: int) -> None:
 
 
 def test_fails_on_fixture_fails_in_finally(fixture_fails_in_finally: int) -> None:
+    pass
+
+
+def test_fails_fixture_failure_does_not_linger_first_iterator(
+    fixture_fails_once_iterator: None,
+) -> None:
+    pass
+
+
+def test_fails_fixture_failure_does_not_linger_second_iterator(
+    fixture_fails_once_iterator: None,
+) -> None:
+    pass
+
+
+def test_fails_fixture_failure_does_not_linger_first_result(
+    fixture_fails_once_result: None,
+) -> None:
+    pass
+
+
+def test_fails_fixture_failure_does_not_linger_second_result(
+    fixture_fails_once_result: None,
+) -> None:
+    pass
+
+
+def test_fails_fixture_failure_does_not_linger_first_finally(
+    fixture_fails_once_finally: None,
+) -> None:
+    pass
+
+
+def test_fails_fixture_failure_does_not_linger_second_finally(
+    fixture_fails_once_finally: None,
+) -> None:
     pass
